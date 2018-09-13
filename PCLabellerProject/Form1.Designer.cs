47214038ab6476039e317a2c663f29d47a2bf169
Maysam Torabi <kherad@gmail.com>
2018-09-13 11:42:43 +0430

    init

---
+++ PCLabellerProject/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace PCLabellerProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Key = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PCLabellerProject.Properties.Resources.Itec_281x206;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // propertyGrid1
+            // 
+            resources.ApplyResources(this.propertyGrid1, "propertyGrid1");
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.PropertySort = System.Windows.Forms.PropertySort.NoSort;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Key,
+            this.Value});
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            // 
+            // Key
+            // 
+            this.Key.Frozen = true;
+            resources.ApplyResources(this.Key, "Key");
+            this.Key.Name = "Key";
+            this.Key.ReadOnly = true;
+            // 
+            // Value
+            // 
+            resources.ApplyResources(this.Value, "Value");
+            this.Value.Name = "Value";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Key;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
+
---
+++ PCLabellerProject/Form1.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PCLabellerProject
+{
+    /*
+    public class KeyValue
+    {
+        public string Key  { get; set; }
+        public string Value { get; set; }
+    }
+     */ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string trabajo_folder;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey UserPrefs = Registry.CurrentUser.OpenSubKey("ITEC", true);
+
+            if (UserPrefs != null)
+            {
+                trabajo_folder = UserPrefs.GetValue("trabajo_folder").ToString();
+//                _Company = UserPrefs.GetValue("Company").ToString();
+  //              _SomeValue = int.Parse(UserPrefs.GetValue("SomeValue").ToString());
+            }
+            else
+            {
+                // Key did not exist so use defaults
+                trabajo_folder = System.Environment.CurrentDirectory;
+    //            _Company = System.Environment.UserDomainName;
+      //          _SomeValue = 0;
+            }
+            /*
+            var key_values = new[] {
+                new KeyValue { Key = "V1", Value = "" },
+                new KeyValue { Key = "V2", Value = "" },
+                new KeyValue { Key = "V3", Value = "" },
+                new KeyValue { Key = "V4", Value = "" },
+                new KeyValue { Key = "V5", Value = "" },
+                new KeyValue { Key = "V6", Value = "" }
+            };
+            dataGridView1.DataSource = key_values;
+             */
+
+            var list = new List<KeyValuePair<string, int>>();
+            list.Add(new KeyValuePair<string, int>("Cat", 1));
+            list.Add(new KeyValuePair<string, int>("Dog", 2));
+            list.Add(new KeyValuePair<string, int>("Rabbit", 4));
+
+            updateFolder(trabajo_folder);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String cmd = "";
+            cmd = cmd + "5000";  // sub HEAD (NOT)
+            cmd = cmd + "00";  //   network number (NOT)
+            cmd = cmd + "FF";  //PLC NUMBER
+            cmd = cmd + "03FF"; // DEMAND OBJECT MUDULE I/O NUMBER
+            cmd = cmd + "00";  //  DEMAND OBJECT MUDULE DEVICE NUMBER
+            cmd = cmd + "001C";//  Length of demand data
+            cmd = cmd + "000A"; //  CPU inspector data
+            cmd = cmd + "0401"; //  Read command (to read the data from PLC we should "0401"
+            cmd = cmd + "0000";//  Sub command
+            cmd = cmd + "D*";//   device code
+            cmd = cmd + "009500"; //adBase 
+            cmd = cmd + "0001";
+            //Device No ,It’s a Address every PLC device will have an address
+            //we need to send the appropriate address to read the data.
+
+        }
+
+        private void updateFolder(string path)
+        {
+            trabajo_folder = path;
+            label2.Text = trabajo_folder;
+            listBox1.Items.Clear();
+            string[] files = Directory.GetFiles(path, "*.prn").Select(Path.GetFileName).ToArray();
+            listBox1.Items.AddRange(files);
+            //DirectoryInfo di = new DirectoryInfo(path);
+            //Files files = di.getFiles("*.prn");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            RegistryKey UserPrefs = Registry.CurrentUser.OpenSubKey("ITEC", true);
+
+            if (UserPrefs == null)
+            {
+                // Value does not already exist so create it
+                UserPrefs = Registry.CurrentUser.CreateSubKey("ITEC");
+            }
+
+            UserPrefs.SetValue("trabajo_folder", trabajo_folder);
+            Application.Exit();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = trabajo_folder;
+            if(folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            updateFolder(folderBrowserDialog1.SelectedPath);
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
